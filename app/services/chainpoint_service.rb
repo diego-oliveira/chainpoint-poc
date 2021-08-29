500aa9b9e72103b7ad59e2659371b4fef5dffc51
Diego Oliveira <oliveira.bsi@gmail.com>
2021-08-28 21:13:29 -0300

    feat: Logger chainpoint error

--- app/services/chainpoint_service.rb
+++ app/services/chainpoint_service.rb
@@ -2,7 +2,10 @@ class ChainpointService
   def self.submit(*hashes)
     response = HTTP.post(ENV['CHAINPOINT_URL'], json: { hashes: hashes })
 
-    raise Error::BusinessError.new('OPS! An error has occurred trying to send information to Chainpoint') if response.code != 200
+    if response.code != 200
+      Rails.logger.error "Error while POST information to chainpoint #{ENV['CHAINPOINT_URL']}. Response: #{response}"
+      raise Error::BusinessError.new('OPS! An error has occurred trying to send information to Chainpoint') if response.code != 200
+    end
 
     JSON.parse(response.to_s)
   end
